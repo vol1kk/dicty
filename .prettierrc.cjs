da8e82cecc2e5ed9cf6b4554d86a3fa583ed8835
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-04 11:23:16 +0300

    feat: added prettier support

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+const config = {
+  "trailingComma": "all",
+  "tabWidth": 2,
+  "semi": true,
+  "printWidth": 80,
+  "spaceBeforeFunctionParen": false,
+  "arrowParens": "avoid",
+  "endOfLine": "auto"
+}
+
+module.exports = config;
