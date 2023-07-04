7b1afd8eda56639a7e0357971cc37d0684ef2c30
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-04 12:38:57 +0300

    feat: added zustand state manager

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,11 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#a445ed",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
