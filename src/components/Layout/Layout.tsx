6e2f62ffd71cbda3494ab4c3e5f3cc2ce09847d9
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-04 13:37:26 +0300

    feat: added ability to change fonts

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,23 @@
 import Header from "~/components/Header/Header";
+import useUserPreferences from "~/store/useUserPreferences";
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
+const FONTS = new Map([
+  ["mono", "font-inconsolata"],
+  ["serif", "font-merriweather"],
+  ["sans-serif", "font-poppins"],
+]);
+
 export default function Layout({ children }: LayoutProps) {
+  const font = useUserPreferences(state => state.font);
+  const getFont = FONTS.get(font) ?? "font-poppins";
   return (
-    <div className="m-auto grid min-h-screen max-w-screen-md grid-rows-[auto,_1fr] p-6">
+    <div
+      className={`m-auto grid min-h-screen max-w-screen-md grid-rows-[auto,_1fr] p-6 ${getFont}`}
+    >
       <Header />
       {children}
     </div>
