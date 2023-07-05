61a5f602b0d5ff09d3abd4b2d6a744d78926bcb3
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-05 13:49:26 +0300

    refactor: main page supports dark theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import clsx from "clsx";
 import Head from "next/head";
 import SearchIcon from "~/components/Icons/SearchIcon";
+import useUserPreferences from "~/store/useUserPreferences";
 
 export default function Home() {
+  const isDarkTheme = useUserPreferences(state => state.theme) === "dark";
+  function inputSearchHandler(e: React.MouseEvent) {
+    e.preventDefault();
+    console.log(1);
+  }
+
   return (
     <>
       <Head>
@@ -10,23 +18,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="mt-10">
-        <div className="relative">
+        <form className="relative">
           <input
-            className="w-full rounded-xl bg-gray-100 p-4 text-lg placeholder:font-bold focus:outline-0 focus-visible:ring-2 focus-visible:ring-primary focus-visible:ring-offset-2"
+            className={`w-full rounded-xl p-4 text-lg outline-2 outline-offset-2 outline-primary placeholder:font-bold focus-visible:outline ${
+              isDarkTheme ? "bg-gray-800" : "bg-gray-100"
+            }`}
             type="text"
             placeholder="Search for your words..."
           />
           <button
-            className="group absolute right-0 top-1/2 -translate-y-1/2 p-4 outline-primary focus-visible:outline-2"
-            type="button"
+            aria-hidden="true"
+            onClick={inputSearchHandler}
+            className="group absolute right-0 top-1/2 -translate-y-1/2 rounded-lg p-3 outline-2 outline-primary focus-visible:outline"
+            type="submit"
           >
-            <label className="sr-only">Search button</label>
             <SearchIcon
+              aria-hidden="true"
               dimensions={30}
-              className="hover:fill-primary group-focus-visible:fill-primary"
+              className={clsx(
+                isDarkTheme && "fill-primary",
+                `hover:fill-primary group-focus-visible:fill-primary`,
+              )}
             />
           </button>
-        </div>
+        </form>
       </main>
     </>
   );
