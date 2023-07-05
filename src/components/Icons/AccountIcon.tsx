967e7225ab01f13f17f23bfb6ba54f7e43ba68ed
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-05 17:27:51 +0300

    feat: added new icons for mobile header

---
+++ src/components/Icons/AccountIcon.tsx
@@ -0,0 +1,19 @@
+import { type Icon } from "~/types/Icon";
+
+export default function AccountIcon({
+  dimensions = 36,
+  fill = "#757575",
+}: Icon) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32 32"
+      id="account"
+      fill={fill}
+      width={dimensions}
+      height={dimensions}
+    >
+      <path d="M10 9a6 6 0 1 1 6 6 6 6 0 0 1-6-6Zm16 20H6a3 3 0 0 1-3-3 9 9 0 0 1 9-9h8a9 9 0 0 1 9 9 3 3 0 0 1-3 3Z"></path>
+    </svg>
+  );
+}
