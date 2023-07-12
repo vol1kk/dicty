ddb3f254ee27d9f12ea7abe58d88a37efe5f6def
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-12 13:38:17 +0300

    feat: show message if no words in dictionary

--- src/components/WordsList/WordsList.tsx
+++ src/components/WordsList/WordsList.tsx
@@ -8,6 +8,7 @@ import WordMeanings from "~/components/Word/WordMeanings";
 import useUserPreferences from "~/store/useUserPreferences";
 import WordEdit from "~/components/Word/WordEdit";
 import { type Word } from "~/utils/placeholder";
+import DictionaryIcon from "~/components/Icons/DictionaryIcon";
 
 type WordsListProps = {
   data: Word[];
@@ -16,6 +17,21 @@ type WordsListProps = {
 export default function WordsList({ data }: WordsListProps) {
   const [isOpen, setIsOpen] = useState<number | null>(null);
   const isDarkTheme = useUserPreferences(state => state.theme) === "dark";
+
+  if (data.length === 0) {
+    return (
+      <div className="flex flex-col items-center gap-4">
+        <div
+          className={`rounded-full ${
+            isDarkTheme ? "bg-gray-800" : "bg-gray-100"
+          } p-6`}
+        >
+          <DictionaryIcon dimensions={64} />
+        </div>
+        <h2>Your dictionary is empty!</h2>
+      </div>
+    );
+  }
 
   return (
     <ul className="[&>li]:mb-4">
