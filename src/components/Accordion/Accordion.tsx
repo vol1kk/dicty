732f66c64f0e2bc570a2f3e1324fe62e7da5ea57
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-12 22:41:06 +0300

    fix: Accordion Component has aria-expanded property

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -8,12 +8,13 @@ type DropdownProps = {
 export default function Accordion({ children, isOpen }: DropdownProps) {
   return (
     <div
+      aria-expanded={isOpen}
       className={clsx(
         isOpen ? "grid-rows-[1fr]" : "grid-rows-[0fr]",
         "grid transition-[grid-template-rows]",
       )}
     >
-      <div className="overflow-hidden ">{children}</div>
+      <div className="overflow-hidden">{children}</div>
     </div>
   );
 }
