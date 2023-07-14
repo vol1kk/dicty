598215775f22afb5e2501392690d4923d948ba4e
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-14 09:56:51 +0300

    refactor: edit category button uses dropdown component

--- src/components/Form/FormCategory/index.tsx
+++ src/components/Form/FormCategory/index.tsx
@@ -1,12 +1,11 @@
+import { useRef } from "react";
 import { FieldArray } from "formik";
-import { useRef, useState } from "react";
 import Input from "~/components/Input/Input";
 import Button from "~/components/Button/Button";
 import DotsIcon from "~/components/Icons/DotsIcon";
+import Dropdown from "~/components/Dropdown/Dropdown";
 import FormMeaning from "~/components/Form/FormMeaning";
-import {
-  type CategoryWithoutId,
-} from "~/utils/placeholder";
+import { type CategoryWithoutId } from "~/utils/placeholder";
 import { type FieldArrayHelpers } from "~/types/FieldArrayHelpers";
 
 type FormCategoryProps = {
@@ -35,7 +34,6 @@ export default function FormCategory({
   const pushMeaningRef = useRef(
     {} as Pick<FieldArrayHelpers, "push" | "remove">,
   );
-  const [isExpanded, setIsExpanded] = useState(false);
 
   function removeCategoryHandler(i: number) {
     if (categoriesLength > 1) removeCategory(i);
@@ -47,46 +45,74 @@ export default function FormCategory({
       removeCategoryHandler(categoryIndex);
   }
 
+  function categoryCallback(data: HTMLLIElement) {
+    const action = data.dataset.action ?? "Add Meaning";
+
+    if (action === "add-meaning") pushMeaningRef.current?.push(meaningTemplate);
+
+    if (action === "add-category") pushCategory(categoryTemplate);
+
+    if (action === "remove-category") removeCategoryHandler(categoryIndex);
+
+    data.ariaSelected = "false";
+    (data.parentElement?.firstElementChild as HTMLLIElement).ariaSelected =
+      "true";
+  }
+
   return (
     <div
       role="list"
       className="mb-8 [&>*]:mb-4"
       key={`categories.${categoryIndex}`}
     >
-      <Input
-        id={`categories.${categoryIndex}.name`}
-        before={false}
-        className="w-full "
-        classNameLabel="relative block col-span-2"
-        value={category.name}
-        placeholder="Enter Category"
-      >
-        <Button
-          onClick={() => setIsExpanded(p => !p)}
-          className="absolute right-2 top-1/2 flex -translate-y-1/2 items-center justify-center rounded-full bg-primary bg-opacity-30 p-2 dark:bg-gray-800"
+      <div>
+        <Input
+          id={`categories.${categoryIndex}.name`}
+          before={false}
+          className="w-full"
+          classNameLabel="relative flex col-span-2 dark:bg-gray-900 bg-gray-300 rounded-md"
+          value={category.name}
+          placeholder="Enter Category"
         >
-          <DotsIcon dimensions={16} className="fill-primary dark:fill-white" />
-        </Button>
-        {isExpanded && (
-          <div
-            onClick={e => e.preventDefault()}
-            className="absolute right-0 top-10 z-10 rounded-md bg-white p-4 shadow-2xl dark:bg-gray-900"
-          >
-            <ul className="[&>li]:cursor-pointer [&>li]:p-2">
-              <li onClick={() => pushMeaningRef.current?.push(meaningTemplate)}>
-                Add Meaning
-              </li>
-              <li onClick={() => pushCategory(categoryTemplate)}>
-                Add Category
-              </li>
-              <li onClick={() => removeCategoryHandler(categoryIndex)}>
-                Remove Category
-              </li>
-            </ul>
-          </div>
-        )}
-      </Input>
-
+          <Dropdown
+            tabIndex={-1}
+            renderTitle={() => (
+              <Button className="rounded-full bg-primary bg-opacity-30 p-2 dark:bg-gray-800">
+                <span className="sr-only">Edit Category</span>
+                <DotsIcon
+                  aria-hidden={true}
+                  dimensions={16}
+                  className="fill-primary dark:fill-white"
+                />
+              </Button>
+            )}
+            renderContent={dropdownItemHandler => {
+              return (
+                <ul
+                  onClick={e => e.preventDefault()}
+                  className="absolute -top-5 right-0 min-w-[180px] rounded-md bg-white p-4 shadow-xl dark:bg-gray-900 [&>li]:cursor-pointer [&>li]:leading-10"
+                >
+                  {["Add Meaning", "Add Category", "Remove Category"].map(
+                    (name, index) => (
+                      <li
+                        key={name}
+                        role="option"
+                        aria-selected={0 === index}
+                        onClick={dropdownItemHandler}
+                        data-action={name.toLowerCase().split(" ").join("-")}
+                        className="outline-2 outline-offset-2 outline-primary hover:text-primary focus-visible:outline aria-selected:text-primary"
+                      >
+                        {name}
+                      </li>
+                    ),
+                  )}
+                </ul>
+              );
+            }}
+            callback={categoryCallback}
+          />
+        </Input>
+      </div>
       <FieldArray name={`categories.${categoryIndex}.meanings`}>
         {(arrayHelpers: FieldArrayHelpers) => {
           pushMeaningRef.current.push = arrayHelpers.push;
