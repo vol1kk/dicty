baf7e1e0d27f81f783d9aa2cda8c15f3324bd957
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-16 22:23:01 +0300

    refactor: downgraded prisma scheme & router to match form (will be changed later)

--- src/server/api/routers/wordRouter.ts
+++ src/server/api/routers/wordRouter.ts
@@ -2,15 +2,14 @@ import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 const meanings = {
-  meaning: z.string(),
-  exampleSentences: z.string().array(),
+  definition: z.string(),
+  example: z.string(),
   categoryId: z.string(),
 };
 
 const categories = {
   name: z.string(),
   meanings: z.object(meanings).array(),
-  synonyms: z.string().array(),
 };
 
 const word = z.object({
@@ -22,20 +21,18 @@ const word = z.object({
 
 export const wordRouter = createTRPCRouter({
   createWord: protectedProcedure.input(word).mutation(({ ctx, input }) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
     return ctx.prisma.word.create({
       data: {
         name: input.name,
+        createdById: input.userId,
         transcription: input.transcription,
-        userId: input.userId,
         categories: {
           create: input.categories.map(category => ({
             name: category.name,
-            synonyms: category.synonyms,
             meanings: {
               create: category.meanings.map(meaning => ({
-                meaning: meaning.meaning,
-                exampleSentences: meaning.exampleSentences,
+                definition: meaning.definition,
+                example: meaning.example,
               })),
             },
           })),
