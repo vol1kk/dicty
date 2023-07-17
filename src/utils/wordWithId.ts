fb30948afd0bfc4a1489ddd2ec33ad7d6e4d2a29
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-17 10:53:28 +0300

    feat: helper that uses nanoid to add ids to word if no id

---
+++ src/utils/wordWithId.ts
@@ -0,0 +1,24 @@
+import { type Word } from "~/utils/placeholder";
+import { nanoid } from "nanoid";
+
+export default function wordWithId(word: Word): Word {
+  const wordId = word.id || nanoid();
+
+  const { categories, ...rest } = word;
+  const moddedCategories = categories.map(category => {
+    const moddedMeanings = category.meanings.map(meaning => ({
+      ...meaning,
+      id: meaning.id || nanoid(),
+      categoryId: category.id || nanoid(),
+    }));
+
+    return { ...category, id: nanoid(), wordId, meanings: moddedMeanings };
+  });
+
+  return {
+    ...rest,
+    id: wordId,
+    categories: moddedCategories,
+    createdById: word.createdById || nanoid(),
+  };
+}
