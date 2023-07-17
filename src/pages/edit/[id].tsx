b934524cd63667245f99e0cbad170122dbfab16d
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-17 14:05:52 +0300

    feat: added head tag to edit page

--- src/pages/edit/[id].tsx
+++ src/pages/edit/[id].tsx
@@ -4,6 +4,7 @@ import Form from "~/components/Form/Form";
 import useWords from "~/hooks/useGetWords";
 import { type Word } from "~/utils/placeholder";
 import Button from "~/components/Button/Button";
+import Head from "next/head";
 
 export default function EditPage() {
   const [words] = useWords();
@@ -28,30 +29,40 @@ export default function EditPage() {
   if (!word) return <h1>placeholder</h1>;
 
   return (
-    <main>
-      <div>
-        <Form
-          initialValues={word}
-          submitHandler={submitHandler}
-          renderButtons={(isValid, handleFormReset) => (
-            <>
-              <Button
-                isSubmit={true}
-                onClick={() => {
-                  if (isValid) setTimeout(handleFormReset, 500);
-                }}
-                className="rounded-md bg-gray-300 dark:bg-gray-900"
-              >
-                Save Changes
-              </Button>
-              <Button onClick={() => navigation.push("/")}>Cancel</Button>
-              <Button onClick={deleteHandler} className="dark:hover:bg-red-500">
-                Del
-              </Button>
-            </>
-          )}
-        />
-      </div>
-    </main>
+    <>
+      <Head>
+        <title>Dicty | {word.name}</title>
+        <meta name="description" content="Create your own dictionary!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>
+          <Form
+            initialValues={word}
+            submitHandler={submitHandler}
+            renderButtons={(isValid, handleFormReset) => (
+              <>
+                <Button
+                  isSubmit={true}
+                  onClick={() => {
+                    if (isValid) setTimeout(handleFormReset, 500);
+                  }}
+                  className="rounded-md bg-gray-300 dark:bg-gray-900"
+                >
+                  Save Changes
+                </Button>
+                <Button onClick={() => navigation.push("/")}>Cancel</Button>
+                <Button
+                  onClick={deleteHandler}
+                  className="dark:hover:bg-red-500"
+                >
+                  Del
+                </Button>
+              </>
+            )}
+          />
+        </div>
+      </main>
+    </>
   );
 }
