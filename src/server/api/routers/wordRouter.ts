b3853ec4c4eb22a54e536e29aa256560b941a560
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-18 14:32:36 +0300

    feat: very basic update function (delete & create again)

--- src/server/api/routers/wordRouter.ts
+++ src/server/api/routers/wordRouter.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
+import { TRPCError } from "@trpc/server";
 
 const MeaningSchema = z.object({
   id: z.string().optional(),
   definition: z.string(),
   example: z.string().optional(),
-  categoryId: z.string().optional(),
 });
 
 const CategorySchema = z.object({
@@ -28,19 +28,7 @@ export const wordRouter = createTRPCRouter({
     .input(WordSchema)
     .mutation(({ ctx, input }) => {
       return ctx.prisma.word.create({
-        data: {
-          name: input.name,
-          createdById: ctx.authedUser.id,
-          transcription: input.transcription,
-          categories: {
-            create: input.categories.map(category => ({
-              name: category.name,
-              meanings: {
-                create: category.meanings,
-              },
-            })),
-          },
-        },
+        data: createWord(input, ctx.authedUser.id),
       });
     }),
 
@@ -58,4 +46,39 @@ export const wordRouter = createTRPCRouter({
         where: { createdById: ctx.authedUser.id, id: input.id },
       });
     }),
+
+  updateWord: protectedProcedure
+    .input(WordSchema)
+    .mutation(async ({ ctx, input: updatedWord }) => {
+      const existingWord = await ctx.prisma.word.findUnique({
+        where: { id: updatedWord.id },
+        include: { categories: { include: { meanings: true } } },
+      });
+
+      if (!existingWord) throw new TRPCError({ code: "NOT_FOUND" });
+
+      if (existingWord.createdById !== updatedWord.createdById)
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+
+      await ctx.prisma.word.delete({ where: { id: existingWord.id } });
+      return ctx.prisma.word.create({
+        data: createWord(updatedWord, ctx.authedUser.id),
+      });
+    }),
 });
+
+function createWord(data: z.infer<typeof WordSchema>, userId: string) {
+  return {
+    name: data.name,
+    createdById: userId,
+    transcription: data.transcription,
+    categories: {
+      create: data.categories.map(category => ({
+        name: category.name,
+        meanings: {
+          create: category.meanings,
+        },
+      })),
+    },
+  };
+}
