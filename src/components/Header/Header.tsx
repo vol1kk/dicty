4b4f1bf18b44baa1473d9b8ae93cabd68f5f36b6
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-20 14:45:51 +0300

    feat: added overlay header menu (to be refactored)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,19 +3,84 @@ import Dictionary from "~/components/Icons/DictionaryIcon";
 
 import Switch from "~/components/Header/Switch";
 import FontDropdown from "~/components/Header/FontDropdown";
+import { signIn, signOut } from "next-auth/react";
+import Button from "~/components/Button/Button";
+import useSessionData from "~/store/useSessionData";
+import { useEffect } from "react";
+import clsx from "clsx";
+import useHeaderData from "~/store/useHeaderData";
 
 export default function Header() {
+  const isAuthed = useSessionData(state => state.isAuthed);
+  const isMenuOpen = useHeaderData(state => state.isHeaderOpen);
+  const setIsMenuOpen = useHeaderData(state => state.setIsHeaderOpen);
+
+  useEffect(() => {
+    const clickCloseHandler = () => {
+      if (isMenuOpen) setIsMenuOpen(false);
+    };
+
+    const keyboardCloseHandler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") setIsMenuOpen(false);
+    };
+
+    document.body.addEventListener("click", clickCloseHandler);
+    document.body.addEventListener("keydown", keyboardCloseHandler);
+    return () => {
+      document.body.removeEventListener("click", clickCloseHandler);
+      document.body.removeEventListener("keydown", keyboardCloseHandler);
+    };
+  }, [isMenuOpen, setIsMenuOpen]);
+
   return (
-    <header className="mb-10 flex items-center justify-between">
-      <Link
-        href="/"
-        className="rounded-md outline-2 outline-offset-2 outline-primary focus-visible:outline"
+    <header className="mb-10">
+      <nav className="relative z-20 grid grid-cols-[auto,_1fr] items-center">
+        <Button
+          onClick={e => {
+            e.stopPropagation();
+            setIsMenuOpen();
+          }}
+          aria-expanded={isMenuOpen}
+          className="group/menu relative flex h-6 w-6 items-center overflow-hidden [&>span]:w-full [&>span]:transition-transform [&>span]:duration-200"
+        >
+          <span className="absolute top-0 h-[2px] bg-gray-400 group-aria-expanded/menu:top-1/2 group-aria-expanded/menu:rotate-45"></span>
+          <span className="absolute h-[2px] bg-gray-400 group-aria-expanded/menu:translate-x-full"></span>
+          <span className="absolute bottom-0 h-[2px] bg-gray-400 group-aria-expanded/menu:top-1/2 group-aria-expanded/menu:-rotate-45"></span>
+        </Button>
+        <Link
+          href="/"
+          className="place-self-center rounded-md outline-2 outline-offset-2 outline-primary focus-visible:outline"
+        >
+          <Dictionary aria-label="Home Icon" />
+        </Link>
+      </nav>
+      <div
+        className={clsx(
+          !isMenuOpen && "invisible translate-x-full",
+          "fixed bottom-0 left-0 right-0 top-0 z-10 bg-gray-300 transition-[transform,_visibility] dark:bg-gray-800",
+        )}
       >
-        <Dictionary aria-label="Home Icon" />
-      </Link>
-      <div className="flex items-center gap-4">
-        <FontDropdown />
-        <Switch />
+        <div className="grid h-full place-content-center place-items-center gap-4 ">
+          <ul
+            onClick={e => e.stopPropagation()}
+            className="grid place-items-center gap-4 text-black dark:text-gray-100"
+          >
+            <li className="[&>button]:rounded-md">
+              {isAuthed ? (
+                <Button onClick={() => void signOut()}>Logout</Button>
+              ) : (
+                <Button onClick={() => void signIn()}>Login</Button>
+              )}
+            </li>
+
+            <li>
+              <FontDropdown />
+            </li>
+            <li>
+              <Switch />
+            </li>
+          </ul>
+        </div>
       </div>
     </header>
   );
--- src/components/Header/Switch/index.tsx
+++ src/components/Header/Switch/index.tsx
@@ -24,9 +24,9 @@ export default function Switch() {
       <span className="relative block h-6 w-12 rounded-full border-4 border-transparent bg-gray-600 outline-2 outline-offset-2 outline-primary peer-focus-visible:outline dark:bg-primary [&~svg]:hover:fill-primary dark:[&~svg]:fill-primary">
         <span className="absolute left-0 h-4 w-4 rounded-full bg-white transition-all dark:left-6" />
       </span>
-      <span className="-rotate-[32deg]">
-        <MoonIcon className="fill-none stroke-[#757575] stroke-[5px] group-hover:stroke-primary dark:stroke-primary" />
-      </span>
+      {/*<span className="-rotate-[32deg]">*/}
+      {/*  <MoonIcon className="fill-none stroke-[#757575] stroke-[5px] group-hover:stroke-primary dark:stroke-primary" />*/}
+      {/*</span>*/}
     </label>
   );
 }
