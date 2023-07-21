2645b43ebaa0943a23c2a74df12b3be56576667c
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-21 21:37:05 +0300

    refactor: using overlay component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,79 +1,69 @@
-import Link from "next/link";
-import Dictionary from "~/components/Icons/DictionaryIcon";
-
-import Switch from "~/components/Header/Switch";
-import FontDropdown from "~/components/Header/FontDropdown";
-import { signIn, signOut } from "next-auth/react";
-import Button from "~/components/Button/Button";
-import useSessionData from "~/store/useSessionData";
-import { useEffect } from "react";
 import clsx from "clsx";
+import Link from "next/link";
+import { signIn, signOut } from "next-auth/react";
+import Switch from "~/components/Header/Switch";
+import Button from "~/components/Button/Button";
 import useHeaderData from "~/store/useHeaderData";
+import Overlay from "~/components/Overlay/Overlay";
+import useSessionData from "~/store/useSessionData";
+import Dictionary from "~/components/Icons/DictionaryIcon";
+import FontDropdown from "~/components/Header/FontDropdown";
 
 export default function Header() {
   const isAuthed = useSessionData(state => state.isAuthed);
-  const isMenuOpen = useHeaderData(state => state.isHeaderOpen);
-  const setIsMenuOpen = useHeaderData(state => state.setIsHeaderOpen);
+  const isHeaderOpen = useHeaderData(state => state.isHeaderOpen);
+  const setIsHeaderOpen = useHeaderData(state => state.setIsHeaderOpen);
 
-  useEffect(() => {
-    document.body.classList.toggle("overflow-hidden");
-    const clickCloseHandler = () => {
-      if (isMenuOpen) setIsMenuOpen(false);
-    };
+  function authenticationHandler() {
+    if (isAuthed) void signOut();
 
-    const keyboardCloseHandler = (e: KeyboardEvent) => {
-      if (e.key === "Escape") setIsMenuOpen(false);
-    };
-
-    document.body.addEventListener("click", clickCloseHandler);
-    document.body.addEventListener("keydown", keyboardCloseHandler);
-    return () => {
-      document.body.removeEventListener("click", clickCloseHandler);
-      document.body.removeEventListener("keydown", keyboardCloseHandler);
-    };
-  }, [isMenuOpen, setIsMenuOpen]);
+    if (!isAuthed) void signIn();
+  }
+  function openMenuHandler(e: React.MouseEvent<HTMLButtonElement>) {
+    e.stopPropagation();
+    setIsHeaderOpen();
+  }
 
   return (
     <header className="mb-10">
       <nav className="relative z-20 grid grid-cols-[auto,_1fr] items-center">
         <Button
-          onClick={e => {
-            e.stopPropagation();
-            setIsMenuOpen();
-          }}
-          aria-expanded={isMenuOpen}
-          className="group/menu relative flex h-6 w-6 items-center overflow-hidden [&>span]:w-full [&>span]:transition-transform [&>span]:duration-200"
+          onClick={openMenuHandler}
+          aria-expanded={isHeaderOpen}
+          className="group/menu relative flex h-6 w-6 items-center overflow-hidden [&>span]:w-full [&>span]:bg-gray-400 [&>span]:transition-transform [&>span]:duration-200 [&>span]:aria-expanded:bg-primary"
         >
-          <span className="absolute top-0 h-[2px] bg-gray-400 group-aria-expanded/menu:top-1/2 group-aria-expanded/menu:rotate-45"></span>
-          <span className="absolute h-[2px] bg-gray-400 group-aria-expanded/menu:translate-x-full"></span>
-          <span className="absolute bottom-0 h-[2px] bg-gray-400 group-aria-expanded/menu:top-1/2 group-aria-expanded/menu:-rotate-45"></span>
+          <span className="sr-only">Open header menu</span>
+          <span className="absolute top-0 h-[2px] group-aria-expanded/menu:top-1/2 group-aria-expanded/menu:rotate-45" />
+          <span className="absolute h-[2px] group-aria-expanded/menu:translate-x-full" />
+          <span className="absolute bottom-0 h-[2px] group-aria-expanded/menu:top-1/2 group-aria-expanded/menu:-rotate-45" />
         </Button>
         <Link
           href="/"
           className="place-self-center rounded-md outline-2 outline-offset-2 outline-primary focus-visible:outline"
         >
-          <Dictionary aria-label="Home Icon" />
+          <Dictionary
+            aria-label="Home Icon"
+            className={clsx(isHeaderOpen && "fill-black dark:fill-gray-100")}
+          />
         </Link>
       </nav>
-      <div
+      <Overlay
+        isOverlayActive={isHeaderOpen}
         className={clsx(
-          !isMenuOpen && "invisible translate-x-full",
-          "fixed bottom-0 left-0 right-0 top-0 z-10 bg-gray-300 transition-[transform,_visibility] dark:bg-gray-800",
+          !isHeaderOpen && "translate-x-full",
+          "transition-[transform,_visibility]",
         )}
       >
-        <div className="grid h-full place-content-center place-items-center gap-4 ">
+        <div className="grid h-full place-items-center">
           <ul
             onClick={e => e.stopPropagation()}
-            className="grid place-items-center gap-4 text-2xl text-black dark:text-gray-100"
+            className="grid place-items-center gap-6 text-3xl"
           >
             <li className="[&>button]:rounded-md">
-              {isAuthed ? (
-                <Button onClick={() => void signOut()}>Logout</Button>
-              ) : (
-                <Button onClick={() => void signIn()}>Login</Button>
-              )}
+              <Button onClick={authenticationHandler}>
+                {isAuthed ? "Logout" : "Login"}
+              </Button>
             </li>
-
             <li>
               <FontDropdown />
             </li>
@@ -82,7 +72,7 @@ export default function Header() {
             </li>
           </ul>
         </div>
-      </div>
+      </Overlay>
     </header>
   );
 }
