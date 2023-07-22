273c985f042ea0ba831f96d18a08115670e8dac3
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-22 13:59:01 +0300

    refactor: overlay by default uses portals & traps focus inside the overlay

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,6 +1,6 @@
-import { createPortal } from "react-dom";
 import clsx from "clsx";
-import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+import { useEffect, useRef, useState } from "react";
 import useHeaderData from "~/store/useHeaderData";
 
 type OverlayProps = {
@@ -14,10 +14,11 @@ export default function Overlay({
   children,
   className,
   isOverlayActive,
-  usePortal = false,
+  usePortal = true,
 }: OverlayProps) {
-  const [_document, setDocument] = useState<null | Document>(null);
+  const overlayRef = useRef<HTMLDivElement>(null);
   const setIsMenuOpen = useHeaderData(state => state.setIsHeaderOpen);
+  const [_document, setDocument] = useState<Document | null>(null);
 
   useEffect(() => {
     setDocument(document);
@@ -29,35 +30,67 @@ export default function Overlay({
     };
 
     const keyboardCloseHandler = (e: KeyboardEvent) => {
+      e.preventDefault();
+
       if (e.key === "Escape") setIsMenuOpen(false);
+
+      const focusableNodes = overlayRef.current?.querySelectorAll(
+        "a, input, button, div[tabindex='0']",
+      );
+
+      if (focusableNodes?.length === 0) return;
+
+      const focusableElements = Array.from(focusableNodes ?? []);
+      const currentFocusIndex = focusableElements.findIndex(
+        e => e === document.activeElement,
+      );
+
+      let nextIndex = currentFocusIndex;
+
+      if (e.key === "Tab") {
+        nextIndex =
+          focusableElements.length - 1 === currentFocusIndex
+            ? 0
+            : nextIndex + 1;
+      }
+
+      if (e.key === "Tab" && e.shiftKey) {
+        nextIndex =
+          currentFocusIndex - 1 < 0
+            ? focusableElements.length - 1
+            : currentFocusIndex - 1;
+      }
+
+      const nextFocusedElement = focusableElements[nextIndex] as HTMLElement;
+      nextFocusedElement?.focus();
     };
 
     document.body.classList.toggle("overflow-hidden");
-    document.body.addEventListener("click", clickCloseHandler);
-    document.body.addEventListener("keydown", keyboardCloseHandler);
+    if (isOverlayActive) {
+      document.body.addEventListener("click", clickCloseHandler);
+      document.body.addEventListener("keydown", keyboardCloseHandler);
+    }
+
     return () => {
       document.body.removeEventListener("click", clickCloseHandler);
       document.body.removeEventListener("keydown", keyboardCloseHandler);
     };
   }, [isOverlayActive, setIsMenuOpen]);
 
-  if (!_document) return null;
-
   const content = (
-    <>
-      <div
-        className={clsx(
-          !isOverlayActive && "invisible",
-          "fixed inset-0 z-10 bg-gray-300 dark:bg-gray-800",
-          className,
-        )}
-      >
-        {children}
-      </div>
-    </>
+    <div
+      ref={overlayRef}
+      className={clsx(
+        !isOverlayActive && "invisible",
+        "fixed inset-0 z-10 text-black dark:text-gray-100",
+        className,
+      )}
+    >
+      {children}
+    </div>
   );
 
-  if (usePortal)
+  if (_document && usePortal)
     return createPortal(
       content,
       _document.getElementById("overlay") as HTMLDivElement,
