6d1837770517649088f1eb694f82f262ee08932d
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-22 14:54:47 +0300

    refactor: created separate component for menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,34 +1,13 @@
 import clsx from "clsx";
 import Link from "next/link";
-import { signIn, signOut } from "next-auth/react";
-import Modal from "~/components/Modal/Modal";
-import Button from "~/components/Button/Button";
 import useHeaderData from "~/store/useHeaderData";
-import Overlay from "~/components/Overlay/Overlay";
-import useSessionData from "~/store/useSessionData";
 import Dictionary from "~/components/Icons/DictionaryIcon";
-import FontDropdown from "~/components/Header/FontDropdown";
-import Switch from "~/components/Switch";
-import useUserPreferences from "~/store/useUserPreferences";
+import HeaderMenu from "~/components/Header/HeaderMenu";
+import ButtonBurger from "~/components/Button/ButtonBurger";
 
 export default function Header() {
-  const setTheme = useUserPreferences(state => state.setTheme);
-  const isAuthed = useSessionData(state => state.isAuthed);
   const setIsHeaderOpen = useHeaderData(state => state.setIsHeaderOpen);
   const isHeaderOpen = useHeaderData(state => state.isHeaderOpen);
-  const isDarkTheme = useUserPreferences(state => state.theme) === "dark";
-
-  function themeToggleHandler() {
-    const calculatedTheme = isDarkTheme ? "light" : "dark";
-    setTheme(calculatedTheme);
-    localStorage.setItem("theme", calculatedTheme);
-  }
-
-  function authenticationHandler() {
-    if (isAuthed) void signOut();
-
-    if (!isAuthed) void signIn();
-  }
 
   function openMenuHandler(e: React.MouseEvent<HTMLButtonElement>) {
     e.stopPropagation();
@@ -38,16 +17,11 @@ export default function Header() {
   return (
     <header className="mb-10">
       <nav className="relative z-20 grid grid-cols-[auto,_1fr] items-center">
-        <Button
-          onClick={openMenuHandler}
-          aria-expanded={isHeaderOpen}
-          className="group/menu relative flex h-6 w-6 items-center overflow-hidden [&>span]:w-full [&>span]:bg-gray-400 [&>span]:transition-transform [&>span]:duration-200 [&>span]:aria-expanded:bg-primary"
-        >
-          <span className="sr-only">Open header menu</span>
-          <span className="absolute top-0 h-[2px] group-aria-expanded/menu:top-1/2 group-aria-expanded/menu:rotate-45" />
-          <span className="absolute h-[2px] group-aria-expanded/menu:translate-x-full" />
-          <span className="absolute bottom-0 h-[2px] group-aria-expanded/menu:top-1/2 group-aria-expanded/menu:-rotate-45" />
-        </Button>
+        <ButtonBurger
+          ariaLabel="Open header menu"
+          isBurgerMenuOpen={isHeaderOpen}
+          openBurgerMenuHandler={openMenuHandler}
+        />
         <Link
           href="/"
           className="place-self-center rounded-md outline-2 outline-offset-2 outline-primary focus-visible:outline"
@@ -58,35 +32,7 @@ export default function Header() {
           />
         </Link>
       </nav>
-      <Overlay
-        isOverlayActive={isHeaderOpen}
-        className={clsx(
-          !isHeaderOpen && "translate-x-full",
-          "bg-gray-300 transition-[transform,_visibility] dark:bg-gray-800",
-        )}
-      >
-        <Modal>
-          <ul
-            onClick={e => e.stopPropagation()}
-            className="grid place-items-center gap-6 p-2 text-3xl"
-          >
-            <li className="[&>button]:rounded-md">
-              <Button onClick={authenticationHandler}>
-                {isAuthed ? "Logout" : "Login"}
-              </Button>
-            </li>
-            <li>
-              <FontDropdown />
-            </li>
-            <li>
-              <Switch
-                isChecked={isDarkTheme}
-                handleCheck={themeToggleHandler}
-              />
-            </li>
-          </ul>
-        </Modal>
-      </Overlay>
+      <HeaderMenu />
     </header>
   );
 }
---
+++ src/components/Header/HeaderMenu/index.tsx
@@ -0,0 +1,58 @@
+import Overlay from "~/components/Overlay/Overlay";
+import clsx from "clsx";
+import Modal from "~/components/Modal/Modal";
+import Button from "~/components/Button/Button";
+import FontDropdown from "~/components/Header/HeaderMenu/FontDropdown";
+import Switch from "~/components/Switch";
+import { signIn, signOut } from "next-auth/react";
+import useUserPreferences from "~/store/useUserPreferences";
+import useSessionData from "~/store/useSessionData";
+import useHeaderData from "~/store/useHeaderData";
+
+export default function HeaderMenu() {
+  const setTheme = useUserPreferences(state => state.setTheme);
+  const isAuthed = useSessionData(state => state.isAuthed);
+  const isHeaderOpen = useHeaderData(state => state.isHeaderOpen);
+  const isDarkTheme = useUserPreferences(state => state.theme) === "dark";
+
+  function themeToggleHandler() {
+    const calculatedTheme = isDarkTheme ? "light" : "dark";
+    setTheme(calculatedTheme);
+    localStorage.setItem("theme", calculatedTheme);
+  }
+
+  function authenticationHandler() {
+    if (isAuthed) void signOut();
+
+    if (!isAuthed) void signIn();
+  }
+
+  return (
+    <Overlay
+      isOverlayActive={isHeaderOpen}
+      className={clsx(
+        !isHeaderOpen && "translate-x-full",
+        "bg-gray-300 transition-[transform,_visibility] dark:bg-gray-800",
+      )}
+    >
+      <Modal>
+        <ul
+          onClick={e => e.stopPropagation()}
+          className="grid place-items-center gap-6 p-2 text-3xl"
+        >
+          <li className="[&>button]:rounded-md">
+            <Button onClick={authenticationHandler}>
+              {isAuthed ? "Logout" : "Login"}
+            </Button>
+          </li>
+          <li>
+            <FontDropdown />
+          </li>
+          <li>
+            <Switch isChecked={isDarkTheme} handleCheck={themeToggleHandler} />
+          </li>
+        </ul>
+      </Modal>
+    </Overlay>
+  );
+}
