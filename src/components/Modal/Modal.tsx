92e595c0347c3e0238849fe809d9bad2e29e0fb7
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-22 14:18:48 +0300

    feat: created modal component

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,11 @@
+type ModalProps = {
+  children: React.ReactNode;
+};
+
+export default function Modal({ children }: ModalProps) {
+  return (
+    <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-3xl">
+      {children}
+    </div>
+  );
+}
