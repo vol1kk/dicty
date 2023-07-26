c3793a352b520b5ec3f3f7df67b28f766eaf2f97
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-26 17:08:16 +0300

    fix 1/2: localWords are stored in zustand store

--- src/store/useUserPreferences.ts
+++ src/store/useUserPreferences.ts
@@ -1,17 +1,22 @@
 import { create } from "zustand";
+import { type Word } from "~/types/ApiTypes";
 
 type useUserPreferences = {
   font: string;
   theme: string;
+  words: Word[];
   setFont: (name: string) => void;
   setTheme: (theme: string) => void;
+  setWords: (words: Word[]) => void;
 };
 
 const useUserPreferences = create<useUserPreferences>()(set => ({
   font: "sans-serif",
   theme: "light",
+  words: [],
   setFont: name => set(() => ({ font: name.toLowerCase() })),
   setTheme: theme => set(() => ({ theme: theme })),
+  setWords: words => set(() => ({ words })),
 }));
 
 export default useUserPreferences;
