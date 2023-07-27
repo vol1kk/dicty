58251e2045b9b0c607d20c385e206c01f6610c2e
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-27 22:08:34 +0300

    fix: fixed keyboard interactions & added key to rerender input

--- src/components/Word/WordShare/index.tsx
+++ src/components/Word/WordShare/index.tsx
@@ -1,6 +1,6 @@
 import Button from "~/components/Button/Button";
 import KeyIcon from "~/components/Icons/KeyIcon";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import CloseIcon from "~/components/Icons/CloseIcon";
 import { api } from "~/utils/api";
 
@@ -11,10 +11,12 @@ type WordShareProps = {
 
 export default function WordShare({ code, wordId }: WordShareProps) {
   const [shareCode, setShareCode] = useState(code);
+  const formCodeRef = useRef<HTMLInputElement>(null);
 
   const { mutate: generateCodeMutation } =
     api.words.generateShareCode.useMutation({
       onSuccess(res) {
+        console.log(res.shareCode);
         setShareCode(res.shareCode as string | null);
       },
     });
@@ -24,27 +26,37 @@ export default function WordShare({ code, wordId }: WordShareProps) {
     },
   });
 
+  function clickHandler() {
+    if (shareCode) deleteCodeMutation({ wordId });
+    if (!shareCode) {
+      generateCodeMutation({ wordId });
+    }
+  }
+
   return (
     <div className="mx-auto mb-1 flex w-fit rounded-md bg-gray-300 p-2 dark:bg-gray-900">
       <input
-        className="mr-2 max-w-[18ch] border-r-[1px] border-r-[#adb2b8] bg-transparent pr-2 outline-0"
-        onClick={e => e.currentTarget.select()}
-        defaultValue={shareCode ?? undefined}
-        placeholder="Generate Word Code"
-        readOnly={true}
         type="text"
+        readOnly={true}
+        ref={formCodeRef}
+        key={shareCode ?? ""}
+        tabIndex={shareCode ? 0 : -1}
+        defaultValue={shareCode ?? ""}
+        placeholder="Generate Word Code"
+        onClick={e => e.currentTarget.select()}
+        className="mr-2 max-w-[18ch] border-r-[1px] border-r-[#adb2b8] bg-transparent pr-2 outline-0"
       />
-      <Button>
+      <Button onClick={clickHandler} className="[&>svg]:fill-primary">
         {shareCode ? (
-          <CloseIcon
-            onClick={() => deleteCodeMutation({ wordId })}
-            className="fill-primary"
-          />
+          <>
+            <span className="sr-only">Delete sharing code</span>
+            <CloseIcon />
+          </>
         ) : (
-          <KeyIcon
-            onClick={() => generateCodeMutation({ wordId })}
-            className="fill-primary"
-          />
+          <>
+            <span className="sr-only">Generate sharing code</span>
+            <KeyIcon />
+          </>
         )}
       </Button>
     </div>
