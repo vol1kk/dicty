0b14665a524816aa89cd53ef2d13526fab6f3976
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-07-31 19:37:39 +0300

    feat: added svgr

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,34 @@ import i18config from "./next-i18next.config.mjs";
 const config = {
   reactStrictMode: true,
   i18n: i18config.i18n,
+
+  webpack(config) {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.(".svg"),
+    )
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ["@svgr/webpack"],
+      },
+    )
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
+    return config
+  },
 };
 
 export default config;
