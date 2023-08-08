c28fb09aa1b562cb187ddaf0a2f716e1e4d719af
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-08-08 12:57:20 +0300

    feat: option to import from code is shown only if authed

--- src/features/word-add/components/FormAddWord.tsx
+++ src/features/word-add/components/FormAddWord.tsx
@@ -6,12 +6,13 @@ import Button from "~/components/Button/Button";
 import FormWord from "~/features/shared/ui/Form";
 import { useCreateWord } from "~/features/word-add";
 import FormImportWord from "~/features/word-add/components/FormImportWord";
+import useSessionData from "~/store/useSessionData";
 
 export default function FormAddWord() {
   const { t } = useTranslation("common");
-
   const [isFormOpen, setIsFormOpen] = useState(false);
   const inputCodeRef = useRef<HTMLInputElement>(null);
+  const isAuthed = useSessionData(state => state.isAuthed);
 
   function formCloseHandler() {
     if (inputCodeRef.current) inputCodeRef.current.value = "";
@@ -35,7 +36,20 @@ export default function FormAddWord() {
         </span>
       </Button>
       <Accordion isOpen={isFormOpen}>
-        <FormImportWord ref={inputCodeRef} closeHandler={formCloseHandler} />
+        {isAuthed && (
+          <>
+            <FormImportWord
+              ref={inputCodeRef}
+              closeHandler={formCloseHandler}
+            />
+            <div className="relative mx-2 flex select-none items-center justify-center">
+              <div className="absolute h-1 w-full rounded-md bg-gray-300 dark:bg-gray-900" />
+              <span className="text-bold relative z-[1] rounded-md bg-gray-300 px-6 py-1 text-sm font-bold uppercase dark:bg-gray-900">
+                {t("form.or")}
+              </span>
+            </div>
+          </>
+        )}
         <FormWord
           submitHandler={word => createWord(word)}
           renderButtons={(isValid, handleFormReset) => {
