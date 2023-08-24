9d26a3bae6d592a3fb6424960a656e43507694b7
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-08-24 09:42:36 +0300

    feat: changed color of undo button for light mode

--- src/components/Button/ButtonUndo.tsx
+++ src/components/Button/ButtonUndo.tsx
@@ -4,7 +4,10 @@ import Button, { type ButtonProps } from "~/components/Button/Button";
 
 export default function ButtonUndo({ children, ...props }: ButtonProps) {
   return (
-    <Button className="rounded-md bg-gray-300 py-1 dark:bg-gray-900" {...props}>
+    <Button
+      className="rounded-md bg-primary bg-opacity-30 py-1 transition-transform dark:bg-gray-900"
+      {...props}
+    >
       {children}
     </Button>
   );
