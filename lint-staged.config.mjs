e6e60e17f1ebe8dc8364896fdfd853b01c7c08ec
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-09-04 18:10:55 +0300

    feat: added husky

---
+++ lint-staged.config.mjs
@@ -0,0 +1,9 @@
+const config = {
+  "**/*.(t|j)s": "next lint",
+  "**/*.(t|j)sx": "next lint",
+}
+
+export default config;
+
+
+
--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,5 +1,11 @@
 /** @type {import("prettier").Config} */
 const config = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  printWidth: 80,
+  arrowParens: "avoid",
+  endOfLine: "auto",
   plugins: [require.resolve("prettier-plugin-tailwindcss")],
 };
 
