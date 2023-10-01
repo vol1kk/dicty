0bd02aba09e23873db19eb3ebc20306f8573add2
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-10-01 21:38:36 +0300

    feat: changed how NotFound looks

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -14,7 +14,12 @@ export default function NotFound({ dimensions, text }: NotFoundProps) {
       <div className="rounded-full bg-gray-100 p-6 dark:bg-gray-800 [&>svg]:fill-black dark:[&>svg]:fill-white">
         <DictionaryIcon height={dimensions} width={dimensions} />
       </div>
-      <h2 data-testid="notfound-text">{text}</h2>
+      <h2
+        data-testid="notfound-text"
+        className="text-center text-xl font-bold uppercase tracking-widest"
+      >
+        {text}
+      </h2>
     </div>
   );
 }
