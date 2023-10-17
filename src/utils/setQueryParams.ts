5a0f812d226bc19153faab641f61121090e5ba83
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-10-17 22:35:24 +0300

    fix: using router.pathname as deliminator to extract query values

--- src/utils/setQueryParams.ts
+++ src/utils/setQueryParams.ts
@@ -6,8 +6,12 @@ export default function setQueryParams(
   value: string | null,
   pathname?: string,
 ) {
+  const deliminator = router.pathname;
+
   // https://nextjs.org/docs/pages/building-your-application/rendering/automatic-static-optimization#how-it-works
-  const params = new URLSearchParams(router.asPath.split("/?")[1] as string);
+  const params = new URLSearchParams(
+    router.asPath.split(deliminator)[1] as string,
+  );
 
   const existingValue = params.get(key);
   if (existingValue === value) return;
