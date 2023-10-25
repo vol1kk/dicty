b57d9dd5a6e489f182e8cd277d72766ab86baf6c
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-10-25 22:44:20 +0300

    feat: updated dependencies and fixed (?) deployment error

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "vitest/config"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @ts-ignore https://github.com/vitejs/vite/issues/13027#issuecomment-1538775351
   plugins: [react()],
   resolve: {
     alias: [{ find: "~", replacement: resolve(__dirname, "./src") }, { find: "#tests", replacement: resolve(__dirname, "./__tests__") }]
