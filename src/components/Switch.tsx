be96beb806b1e1bd22e887dda0b561ac743f2f36
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-10-25 21:05:15 +0300

    fix: active state depends on isChecked as it should have been from the begining

--- src/components/Switch.tsx
+++ src/components/Switch.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import cn from "~/utils/cn";
 
 export type SwitchProps = {
   before?: boolean;
@@ -29,9 +30,14 @@ export default function Switch({
         {switchAction}
       </button>
       {before && children}
-      <span className="relative block h-6 w-12 rounded-full border-4 border-transparent bg-gray-600 outline-2 outline-offset-2 outline-primary peer-focus-visible:outline dark:bg-primary [&~svg]:hover:fill-primary dark:[&~svg]:fill-primary">
-        <span className="absolute left-0 h-4 w-4 rounded-full bg-white transition-all dark:left-6" />
-      </span>
+      <div className="relative block h-6 w-12 rounded-full border-4 border-transparent bg-gray-600 outline-2 outline-offset-2 outline-primary peer-focus-visible:outline dark:bg-primary [&~svg]:hover:fill-primary dark:[&~svg]:fill-primary">
+        <span
+          className={cn(
+            "absolute left-0 h-4 w-4 rounded-full bg-white transition-all",
+            isChecked && "left-6",
+          )}
+        />
+      </div>
       {!before && children}
     </label>
   );
