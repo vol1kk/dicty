2a86b387ebff2808bbbdc3ba3166ecaaf7ef168e
Vsevolod Ruzhytskyi <thebrokennworld@gmail.com>
2023-10-26 00:01:04 +0300

    fix: optmistic word creation fixed

--- src/features/word-edit/utils/getModifiedWords.ts
+++ src/features/word-edit/utils/getModifiedWords.ts
@@ -1,13 +1,27 @@
-import { type Word } from "~/types/ApiTypes";
+import { type Word, WordSchema } from "~/types/ApiTypes";
 import modifyWordId from "~/utils/modifyWordId";
 
 export default function getModifiedWords(
   words: Word[],
   modifiedWord: Partial<Word>,
 ): Word[] {
+  const existingWord = words.find(word => word.id === modifiedWord.id);
+
+  // For creating words
+  if (!existingWord && isNotPartial(modifiedWord)) {
+    return [modifyWordId(modifiedWord), ...words];
+  }
+
+  // For updating words
   return words.map(word =>
     word.id === modifiedWord.id
       ? modifyWordId({ ...word, ...modifiedWord }, { appendWithId: true })
       : word,
   );
+}
+
+export function isNotPartial(word: Partial<Word>): word is Word {
+  const parsedWord = WordSchema.safeParse(word);
+
+  return parsedWord.success;
 }
